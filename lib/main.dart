5b982d94bf7ae12d58faa9bc8aacd2b3197f7d49
Khemraj Saud <khemrajsaud56@gmail.com>
2025-08-04 16:07:00 +0545

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,3 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'screens/weather_screen.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   if (kIsWeb) {
-//     await Firebase.initializeApp(
-//       options: const FirebaseOptions(
-//         apiKey: "AIzaSyDek-svLjTBUpzIMlBT_bq-e_QMOYCidCQ",
-//         authDomain: "project-1ab36.firebaseapp.com",
-//         projectId: "project-1ab36",
-//         storageBucket: "project-1ab36.appspot.com",
-//         messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//         appId: "YOUR_APP_ID",
-//       ),
-//     );
-//   } else {
-//     await Firebase.initializeApp();
-//   }
-
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'WeatherWise',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         fontFamily: 'Roboto',
-//         scaffoldBackgroundColor: Colors.white,
-//       ),
-//       home: WeatherScreen(),
-//     );
-//   }
-// }
-
-
 
 
 import 'package:flutter/foundation.dart';
@@ -75,7 +32,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WeatherWise',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -105,4 +61,3 @@ class AuthWrapper extends StatelessWidget {
     );
   }
 }
-
